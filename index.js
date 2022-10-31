e3d04c76e0d43ac9566865e0e6a6e49dda1fa520
nataliia <102330868+natalikulybina@users.noreply.github.com>
2022-10-31 14:17:15 +0200

    add files

---
+++ index.js
@@ -0,0 +1,24 @@
+const request = new XMLHttpRequest();
+
+
+request.open('GET', 'https://users-api-id.herokuapp.com/users');
+
+request.send();
+
+request.onload = () => {
+  const reply = JSON.parse(request.response);
+  reply.forEach(renderItem);
+};
+
+const list = document.querySelector("ul");
+
+function renderItem({ name, photo, position }) {
+  const listItem = document.createElement("li");
+
+  listItem.innerHTML = `
+    <h2>${name}</h2>
+    <img src="${photo}">
+    <p>${position}</p>
+    `,
+    list.append(listItem);
+}
